b98687a8c5b0ad3e8f6fc199e3c0738690868579
Kevin O'Neal <oneal.kevin@gmail.com>
2014-07-05 16:06:04 -0500

    dumb test

---
+++ src/functional/java/com/scuilion/dependencygrapher/BasicUsageTest.java
@@ -0,0 +1,12 @@
+package com.scuilion.dependencygrapher.core;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BasicUsageTest{
+
+    @Test
+    public void firstTest(){
+        assertTrue(true);
+    }
+}
