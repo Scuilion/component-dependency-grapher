5c233a13eb286b702a20825b44aa43d874e296ad
Kevin O'Neal <oneal.kevin@gmail.com>
2014-08-17 10:46:33 -0500

    rearrange

--- src/main/java/com/scuilion/dependencygrapher/core/DependencyGrapher.java
+++ src/main/java/com/scuilion/dependencygrapher/core/DependencyGrapher.java
@@ -1,6 +1,5 @@
 package com.scuilion.dependencygrapher.core;
 
-import com.scuilion.dependencygrapher.neo4j.node.Creator;
 import com.scuilion.dependencygrapher.core.Utils;
 
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -30,65 +29,6 @@ public class DependencyGrapher{
     }
 
     public void store(Artifacts artifacts){
-        try ( Transaction tx = graphDb.beginTx() ) {
-            for(Artifact artifact : artifacts.getArtifacts().values()){
-                Node node = graphDb.createNode();
-                node.setProperty("artifactName", artifact.getArtifactName());
-                node.setProperty("identifier", artifact.getIdentifier());
-                node.setProperty("language", artifact.getLanguage());
-                node.setProperty("type", artifact.getType());
-            }
-            tx.success();
-        }catch(Exception e){
-            System.out.println("exception thrown" + e.toString());
-        }
-    
-    }
-
-    void createDb()//Artifacts artifacts)
-    {
-        try ( Transaction tx = graphDb.beginTx() )
-        {
-            //Label languageLabel = DynamicLabel.label("java");
-            //Label scopeLabel = DynamicLabel.label("internal");
-            //
-            Artifact artifactOne = new Artifact
-                .ArtifactBuilder("jar1", "com.scuilion.product.featureone")
-                .type("internal")
-                .language("groovy")
-                .build();
-             Artifact artifactTwo = new Artifact
-                .ArtifactBuilder("jarX", "com.scuilion.product.feature.c")
-                .type("internal")
-                .language("java")
-                .build();
-              Artifact artifactThree = new Artifact
-                .ArtifactBuilder("three", "org.thridparty.some.other.thing")
-                .type("external")
-                .language("scala")
-                .build();
-
-            /*{ "jar3":{
-                identifier:"com.scuilion.jarI",
-                language:"groovy",
-                type:"internal"
-                }
-            }
-            { "jar3":{
-                dependsOn:["jar2"],
-                becauseOf:["com.scuilion.provides.api.c.Method1","com.scuilion.provides.api.c.Method2"]
-            }*/
-
-            //relationship = secondNode.createRelationshipTo( thirdNode, RelTypes.DEPENDS_ON );
-            //relationship.setProperty( "becauseOf", "com.scuilion.provides.api.c.Method1" );
-            //relationship.setProperty( "becauseOf", "com.scuilion.provides.api.c.Method2" );
-            // END SNIPPET: addData
-
-            tx.success();
-        }catch(Exception e){
-            System.out.println("exception thrown" + e.toString());
-        }
-        // END SNIPPET: transaction
     }
 
 
--- src/main/java/com/scuilion/dependencygrapher/neo4j/node/Creator.java
+++ src/main/java/com/scuilion/dependencygrapher/neo4j/node/Creator.java
@@ -4,27 +4,48 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.DynamicLabel;
 import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Transaction;
+
+import com.scuilion.dependencygrapher.core.Artifacts;
+import com.scuilion.dependencygrapher.core.Artifact;
 
 import java.util.List;
 import java.util.HashMap;
 
 public class Creator{
 
-    static public void createMultipleNodes(GraphDatabaseService graphDb , List<HashMap<String, Object>> nodes){
-        for(HashMap<String, Object> node : nodes){
-            createNode(graphDb, node);
+    static public void createMultipleNodes(GraphDatabaseService graphDb , Artifacts artifacts){
+        try ( Transaction tx = graphDb.beginTx() ) {
+            for(Artifact artifact : artifacts.getArtifacts().values()){
+                createNode(graphDb, artifact);
+            }
+            tx.success();
+        }catch(Exception e){
+            System.out.println("exception thrown" + e.toString());
         }
     }
 
     @SuppressWarnings("unchecked")
-    static public Node createNode(GraphDatabaseService graphDb , HashMap<String, Object> setupInfo){
+    static public Node createNode(GraphDatabaseService graphDb , Artifact artifact){
+        Node node = graphDb.createNode();
+        node.setProperty("artifactName", artifact.getArtifactName());
+        node.setProperty("identifier", artifact.getIdentifier());
+        node.setProperty("language", artifact.getLanguage());
+        node.setProperty("type", artifact.getType());
+        /*
         Node node = graphDb.createNode();
         node.setProperty("artifactName", setupInfo.get("artifactName"));
         node.setProperty("identifier", setupInfo.get("identifier"));
         node = addLabels(node, (List)setupInfo.get("lables"));
+        */
         return node;
     }
 
+    //relationship = secondNode.createRelationshipTo( thirdNode, RelTypes.DEPENDS_ON );
+    //relationship.setProperty( "becauseOf", "com.scuilion.provides.api.c.Method1" );
+    //relationship.setProperty( "becauseOf", "com.scuilion.provides.api.c.Method2" );
+    // END SNIPPET: addData
+    
     static private Node addLabels(Node node, List<String> labels){
         for(String lable: labels){
             Label dynamicLable = DynamicLabel.label(lable);
